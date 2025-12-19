dba25f9ae8e588b09f2ad17778e050e208ead480
Sylvain Losey <sylvainlosey@gmail.com>
2025-12-19 10:24:23 +0100

    Add fastify

---
+++ apps/api/src/index.ts
@@ -0,0 +1,22 @@
+import Fastify from 'fastify';
+import { version } from '@sungaze/core';
+
+const fastify = Fastify({ logger: true });
+
+fastify.get('/', async () => {
+  return { 
+    status: 'ok', 
+    engineVersion: version // Testing the link to @sungaze/core
+  };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3001, host: '0.0.0.0' });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,2 @@
 export const GET_SUN_INFO = () => "Solar data from Core";
+export const version = "1.0.0";
